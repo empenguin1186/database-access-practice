41a9b631408f05504eda605b40bf728df188687c
empenguin1186 <emperorpenguint1186@gmail.com>
2019-06-22 19:11:42 +0900

    :+1: 戻り値の型を変更

--- src/main/java/jp/co/penguin/emperor/databaseaccess/controller/MainController.java
+++ src/main/java/jp/co/penguin/emperor/databaseaccess/controller/MainController.java
@@ -5,38 +5,40 @@ import jp.co.penguin.emperor.databaseaccess.mapper.ArticleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(path="/")
 public class MainController {
 
     @Autowired
-    ArticleMapper mapper;
+   private ArticleMapper artlcleMapper;
 
     @RequestMapping(path="articles", method = RequestMethod.GET)
-    public String findAll(@RequestParam(required = false) String keyword) {
+    public List<Article> findAll(@RequestParam(required = false) String keyword) {
         if (keyword == null) {
-            return mapper.findAll().toString();
+            return artlcleMapper.findAll();
         }
-        return mapper.findByKeyWord(keyword).toString();
+        return artlcleMapper.findByKeyWord(keyword);
     }
 
     @RequestMapping(path="articles/{id}", method=RequestMethod.GET)
-    public String findOne(@PathVariable int id) {
-        return mapper.findOne(id).toString();
+    public Article findOne(@PathVariable int id) {
+        return artlcleMapper.findOne(id);
     }
 
     @RequestMapping(path="articles/create", method=RequestMethod.POST)
-    public String insert(@RequestBody Article article) {
-        return String.valueOf(mapper.insert(article));
+    public boolean insert(@RequestBody Article article) {
+        return artlcleMapper.insert(article);
     }
 
     @RequestMapping(path="articles/{id}/delete", method=RequestMethod.POST)
-    public String delete(@PathVariable int id) {
-        return String.valueOf(mapper.delete(id));
+    public boolean delete(@PathVariable int id) {
+        return artlcleMapper.delete(id);
     }
 
-    @RequestMapping(path="articles/{id}/update", method=RequestMethod.POST)
-    public String update(@PathVariable int id, @RequestBody Article article) {
-        return String.valueOf(mapper.update(id, article.getContent(), article.getAuthor()));
+    @RequestMapping(path="articles/{id}/update", method=RequestMethod.PUT)
+    public boolean update(@PathVariable int id, @RequestBody Article article) {
+        return artlcleMapper.update(id, article.getContent(), article.getAuthor());
     }
 }
